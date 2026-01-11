1fe52c6d45dd2b22240b4bc34728c98ad1d76e66
i8wg <omarmetwally47@gmail.com>
2026-01-11 13:31:01 +0200

    Installed react-router-dom and successfully built project for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
+// --- ADD THIS IMPORT ---
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* --- WRAP APP COMPONENT WITH HASHROUTER --- */}
+    <HashRouter> 
+      <App />
+    </HashRouter>
+    {/* ------------------------------------------- */}
   </React.StrictMode>,
 )
