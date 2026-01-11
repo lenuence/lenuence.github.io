bde94499c2f37f52b97bbbe9e85f57b6db837070
i8wg <omarmetwally47@gmail.com>
2026-01-11 14:22:23 +0200

    Removed routing wrappers from main.jsx to prevent initial load redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/index.css'
-// --- ADD THIS IMPORT ---
-import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* --- WRAP APP COMPONENT WITH HASHROUTER --- */}
-    <HashRouter> 
-      <App />
-    </HashRouter>
-    {/* ------------------------------------------- */}
+    <App />
   </React.StrictMode>,
-)
+);
